fd907ec5cbab2fd2acb549eb20a953bd8a5d35ab
ArtyomButrim <artyom.butrim.2000@gmail.com>
2020-09-28 14:31:52 +0300

    Create project and add function for generation text file

---
+++ Lab-1/ConsoleSorter/ConsokeSorter/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using TextGenerator;
+
+namespace ConsokeSorter
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            TextGenerator.Generator.GenerateTextFileBySize(1024*1024*1024, "d:/test.txt");
+            Console.WriteLine("Complete");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Lab-1/ConsoleSorter/TextGenerator/Generator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Diagnostics.SymbolStore;
+
+namespace TextGenerator
+{
+    public class Generator
+    {
+        public static async Task GenerateTextFileBySize(long fileSize, string directoryForCreation)
+        {
+            char[] symbols = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+
+            using (var fs = new FileStream(directoryForCreation, FileMode.OpenOrCreate))
+            using (var sw = new StreamWriter(fs,Encoding.Default))
+            {
+                var Random = new Random();
+                long size;
+
+                while ((size = new System.IO.FileInfo(directoryForCreation).Length) < fileSize)
+                {
+                    int stringLength = Random.Next(20, 400);
+
+                    for (int i= 0; i < stringLength; i++ )
+                    {
+                        sw.Write(symbols[Random.Next(0, symbols.Length - 1)]);
+                    }
+                    sw.Write('\n');
+                }
+            }
+        }
+    }
+}
